b0497bb147a48d7ea95e1688f2a13b64d8dbb68f
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 10:51:45 +0200

    Get minecraft file

---
+++ get_minecraft_file.py
@@ -0,0 +1,32 @@
+import os
+import json
+import shutil
+
+path = os.getenv('APPDATA') + "/.minecraft/assets"
+versions = [
+    i.rstrip(".json") for i in os.listdir(path + "/indexes")
+    if i.startswith("1.")
+]
+print("Available versions:", *versions, sep="\n")
+while True:
+    user_input = input("Select a version: ")
+    selection = user_input if user_input else "1.18"
+    if selection in versions:
+        break
+    print("Version not available")
+
+with open(f"{path}/indexes/{selection}.json") as f:
+    objects = json.load(f)['objects']
+
+while True:
+    objectpath = "minecraft/" + input("Enter object path: ")
+    if objectpath in objects:
+        object = objects[objectpath]
+        del objects
+        break
+    print("Object not found: " + objectpath)
+
+src = f"{path}/objects/{object['hash'][:2]}/{object['hash']}"
+dest = f"./files/{objectpath[objectpath.rfind('/') + 1:]}"
+
+shutil.copyfile(src, dest)
